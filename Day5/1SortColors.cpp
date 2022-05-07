7cddd47af75d7ce94e85900abc4b21c6708a98a1
Aditya Pandey <adityapandey107@gmail.com>
2022-05-08 00:11:33 +0530

    Day5

---
+++ Day5/1SortColors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int len = nums.size(),zero=0,one=0,two=0;
+        for(int i=0;i<len;i++)
+        {
+            if(nums[i]==0)
+            {
+                zero++;
+            }
+            else if(nums[i]==1)
+            {
+                one++;
+            }
+            else
+            {
+                two++;
+            }
+        }
+        
+        for(int j=0;j<len;j++)
+        {
+            if(j<zero)
+                nums[j]=0;
+            else if(j<zero+one)
+                nums[j]=1;
+            else 
+                nums[j]=2;
+        }
+    }
+};
